429fd62c38fa27bc46fba15c81ccba659c93bf3b
Andreea Hanson <andreea.hanson7@gmail.com>
2019-03-25 17:46:48 -0600

    Fixed total guesses

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ var errorNum2 = document.querySelector('.max-error');
 var guessError1 = document.querySelector('.guess-error-number1');
 var guessError2 = document.querySelector('.guess-error-number2');
 var totalGuesses = document.querySelector('#total-guesses');
+var x = 0;
 
 
 //creating random number
@@ -98,63 +99,67 @@ function adjustRangesUponWin() {
 	var changeMinRange = updateMinRange.innerHTML;
 	var changeMaxRange = updateMaxRange.innerHTML;
 	updateMinRange.innerHTML = parseInt(changeMinRange) - 10;
-	updateMaxRange.innerHTML = parseInt(changeMaxRange) + 10;
+  updateMaxRange.innerHTML = parseInt(changeMaxRange) + 10;
 }
 // . create one button that submits the players guesses.
-//			i) link the button "click" to submit to correct places.
+//      i) link the button "click" to submit to correct places.
 submitGuessButton.addEventListener('click', function challengerInfo() {
-	// console.log(challenger1Name.value)
-	var c1Name = challenger1Name.value;
-	updateChallenger1Name.innerHTML = c1Name;
-	// console.log(challenger2Name.value)
-	var c2Name = challenger2Name.value;
-	updateChallenger2Name.innerHTML = c2Name;
-	// console.log(guess1.value)
-	var c1Guess = guess1.value;
-	
-	// console.log(guess2.value)
-	var c2Guess = guess2.value;
-	
-	enableButtons();
+// console.log(x)
+  // console.log(challenger1Name.value)
+  var c1Name = challenger1Name.value;
+  updateChallenger1Name.innerHTML = c1Name;
+  // console.log(challenger2Name.value)
+  var c2Name = challenger2Name.value;
+  updateChallenger2Name.innerHTML = c2Name;
+  // console.log(guess1.value)
+  var c1Guess = guess1.value;
+  
+  // console.log(guess2.value)
+  var c2Guess = guess2.value;
+  
+  enableButtons();
 
 
 
-	if (parseInt(c1Guess) === randomNumber) {
-		challenger1Results.innerHTML = "BOOM!";
-		winningCard.innerHTML = `<h3>CHALLENGER 1 
-			<span id='challenger-1-name'>${challenger1Name.value}</span> 
-			<span>vs</span> CHALLENGER 2 <span id='challenger-2-name'>
-			${challenger2Name.value}</span></h3>
-			<h2>CHALLENGER <span id='the-winner-id-number'>2</span> 
-			<span id='challenger-2-name'>${challenger1Name.value}</span></h2>
-			<h2 class='winner'>WINNER</h2>
-			<div class='bottom-winning-card'>
-			<div class='guess-amount'>
-			<h3><span>47</span> GUESSES</h3>
-			</div>
-			<div class='time'>
-			<h3><span>1.35</span> MINUTES</h3>
-			</div>
-			<div class='closing-button'>X</div>
-			</div>`;
-			adjustRangesUponWin()
-	} else if (parseInt(c1Guess) > randomNumber) {
-		challenger1Results.innerHTML = "That's too high!"
-    showGuessesTotal();
-	} else {
-		challenger1Results.innerHTML = "That's too low!"
-    showGuessesTotal();
-	}
-//			i) will need to create an innerHTML to link to
-//				HTML that needs to be changed.
-//		b) if the players guess is too low, 
-//			return a statement.
-//			i) will need to create an innerHTML to link to
-//				HTML that needs to be changed.
-//		c) if the players guess matches the number, 
-//			return a statement.
-//			i) will need to create an innerHTML to link to
-//				HTML that needs to be changed.
+  if (parseInt(c1Guess) === randomNumber) {
+    challenger1Results.innerHTML = "BOOM!";
+    winningCard.innerHTML = `<h3>CHALLENGER 1 
+      <span id='challenger-1-name'>${challenger1Name.value}</span> 
+      <span>vs</span> CHALLENGER 2 <span id='challenger-2-name'>
+      ${challenger2Name.value}</span></h3>
+      <h2>CHALLENGER <span id='the-winner-id-number'>2</span> 
+      <span id='challenger-2-name'>${challenger1Name.value}</span></h2>
+      <h2 class='winner'>WINNER</h2>
+      <div class='bottom-winning-card'>
+      <div class='guess-amount'>
+      <h3><span>47</span> GUESSES</h3>
+      </div>
+      <div class='time'>
+      <h3><span>1.35</span> MINUTES</h3>
+      </div>
+      <div class='closing-button'>X</div>
+      </div>`;
+      adjustRangesUponWin()
+     
+  } else if (parseInt(c1Guess) > randomNumber) {
+    challenger1Results.innerHTML = "That's too high!"
+    // showGuessesTotal();
+    x++
+  } else {
+    challenger1Results.innerHTML = "That's too low!"
+    // showGuessesTotal();
+    x++
+  }
+//      i) will need to create an innerHTML to link to
+//        HTML that needs to be changed.
+//    b) if the players guess is too low, 
+//      return a statement.
+//      i) will need to create an innerHTML to link to
+//        HTML that needs to be changed.
+//    c) if the players guess matches the number, 
+//      return a statement.
+//      i) will need to create an innerHTML to link to
+//        HTML that needs to be changed.
 
 	if (parseInt(c2Guess) === randomNumber) {
 		challenger2Results.innerHTML = "BOOM!";
@@ -179,10 +184,12 @@ submitGuessButton.addEventListener('click', function challengerInfo() {
 			adjustRangesUponWin()
 	} else if (parseInt(c2Guess) > randomNumber) {
 		challenger2Results.innerHTML = "That's too high!"
-    showGuessesTotal();
+    // showGuessesTotal();
+    x++
 	} else {
 		challenger2Results.innerHTML = "That's too low!"
-    showGuessesTotal();
+    // showGuessesTotal();
+    x++
 	}
 
 //player guesses should only fall between min and max range
@@ -217,6 +224,8 @@ submitGuessButton.addEventListener('click', function challengerInfo() {
 	// if(!isNaN(parseInt(c2Guess))) {
 	// 	guess2NaN.innerHTML = 'Please enter a valid number.';
 	// }
+  console.log('should increment ' + x)
+  totalGuesses.innerHTML = x;
 })	
 
 
@@ -249,9 +258,7 @@ clearGameButton.addEventListener('click', function clearGame(event) {
     updateChallenger2Name.innerText = "Name";
     updateChallenger1Guess.innerText = "0";
     updateChallenger2Guess.innerText = "0";
-    totalGuesses.innerHTML = "0";
     event.preventDefault();
-    x = 0;
 });
 
 
@@ -301,29 +308,6 @@ function enableButtons() {
 };
 
 
-// Combined guess count of both players is displayed
-var x = 0;
-var array = Array();
-
-function add_element_to_array() {
-  
-  array[x] = guess1.value;
-  array[x] = guess2.value;
-  x++;
-}
-
-function display_array() {
-  var guessOrder = array.length;
-  for (var y=0; y<array.length; y++) { 
-  };
-  totalGuesses.innerHTML = guessOrder;
-  console.log('The array length is ' +guessOrder);
-}
-
-function showGuessesTotal() {
-  add_element_to_array();
-  display_array();
-}
 
 
 
